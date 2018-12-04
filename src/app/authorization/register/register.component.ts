8c203db8fc670e5ac2b5814207ddd584ed5aabfd
Avinash Thakur <avinash.thakur@debugged-pro.com>
2018-12-05 01:47:09 +0530

    Working on register
    Signed-off-by: Avinash Thakur <avinash.thakur@debugged-pro.com>

--- src/app/authorization/register/register.component.ts
+++ src/app/authorization/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, Validators ,FormControl } from '@angular/forms';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   templateUrl: 'register.component.html'
@@ -7,13 +7,21 @@ import { FormGroup, Validators ,FormControl } from '@angular/forms';
 export class RegisterComponent {
   regdForm :FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
   ngOnInit() {
-    this.regdForm = new FormGroup({
-     PhoneNumber: new FormControl ('', Validators.compose([Validators.required, Validators.minLength(10), Validators.maxLength(10)])),
-     password:new FormControl ('', Validators.required)
+    this.regdForm = this.formBuilder.group ({
+     PhoneNumber:  ['', Validators.compose([Validators.required, Validators.minLength(10), Validators.maxLength(10)])],
+     Email:['', Validators.required,Validators.minLength(10), Validators.maxLength(50)],
+     Password:  ['', Validators.required],
+     Repeatpassword:['',Validators.required]
   });
   }
+  get f() { return this.regdForm.controls; }
 
+  onSubmit(){
+
+  }
 }
 
