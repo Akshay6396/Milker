a586f0fa43aed9853f3aaa6d6625151e0572ce7f
Akshay <akshay.sharma@debugged-pro.com>
2018-12-08 03:23:54 +0530

    reset password

--- src/app/authorization/forgot/forgot.component.ts
+++ src/app/authorization/forgot/forgot.component.ts
@@ -7,10 +7,13 @@ import { AlertService, AuthenticationService } from '../../service/index';
 
 export class ForgotComponent implements OnInit {
   forgot: FormGroup;
+  OTP: FormGroup;
+  reset: FormGroup;
   loading = false;
   submitted = false;
   returnUrl: string;
-  show:boolean=false;
+  show: boolean = false;
+  confirm: boolean = false;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -27,6 +30,13 @@ export class ForgotComponent implements OnInit {
     this.forgot = this.formBuilder.group({
       PhoneNumber: ['', Validators.compose([Validators.required, Validators.minLength(10), Validators.maxLength(10)])],
     });
+    this.OTP = this.formBuilder.group({
+      Otp: ['', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(6)])],
+    });
+    this.reset = this.formBuilder.group({
+      Password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    });
     debugger;
     // reset login status
     // this.authenticationService.logout();
@@ -35,11 +45,23 @@ export class ForgotComponent implements OnInit {
     // this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
 
-  get f() { return this.forgot.controls; }
+  get f() { return this.forgot.controls.PhoneNumber; }
+  get o() { return this.OTP.controls.Otp; }
+  get r() { return this.reset.controls; }
 
   onSubmit() {
     debugger;
-    localStorage.setItem('otp', '123');
+    localStorage.setItem('otp', '123456');
     this.show = true;
+  }
+  onOtp() {
+    debugger;
+    const Otp = localStorage.getItem('otp');
+    const value = this.o.value;
+    if (Otp == value) {
+      this.confirm = true
+    }
+    this.OTP.reset()
+
   }
 }
